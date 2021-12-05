f752da54826910df3d17daa14fe85384e1dbb05a
Robert Elliot <rob@lidalia.org.uk>
2021-12-05 11:54:34 +0000

    Boilerplate around a gradle kotlin native build

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("multiplatform") version "1.5.32"
+}
+
+group = "uk.org.lidalia"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+}
